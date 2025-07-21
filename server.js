548ba5849db831c4efb372ab1879b1951a4860cc
JamesYon <trickymoon01@gmail.com>
2025-07-21 17:02:07 -0400

    build car new page

--- server.js
+++ server.js
@@ -21,6 +21,11 @@ app.get('/', async (req, res) => {
     res.render('index.ejs');
 });
 
+// GET /cars/new
+app.get('/cars/new', (req, res) => {
+    res.render('cars/new.ejs');
+});
+
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
